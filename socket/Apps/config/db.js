0013d5a27f4a78983432e8e37ea6718dc8fd1c52
LeHuyenTT <lethithuhuyen1710@gmail.com>
2024-07-06 10:25:09 +0700

    edit db

--- socket/Apps/config/db.js
+++ socket/Apps/config/db.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose')
 const DBconnection = async() => {
     const conn = await mongoose
         .connect("mongodb+srv://admin:admin@hybridclass.iemw7ln.mongodb.net/HSM?retryWrites=true&w=majority", {
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true, // if using cloud => true
-            // useCreateIndex: true,
-            // useFindAndModify: false
+            useNewUrlParser: true,
+            useUnifiedTopology: true, // if using cloud => true
+            useCreateIndex: true,
+            useFindAndModify: false
         })
         .catch(err => {
             console.log(`Can't connect to the DB`.red, err)
